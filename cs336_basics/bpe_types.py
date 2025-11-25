43575a33a07661c9376c98ff0db3107dd785c2d6
breezing <542538469@qq.com.com>
2025-11-26 01:14:41 +0800

    refactor

---
+++ cs336_basics/bpe_types.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SimplePair:
+  first: bytes
+  second: bytes
+
+  def concat(self) -> bytes:
+    return self.first + self.second
+
+  def __eq__(self, other: "SimplePair") -> bool:
+    return self.first == other.first and self.second == other.second
+
+  def __hash__(self) -> int:
+    return hash((self.first, self.second))
+
+  def to_pair(self) -> tuple[bytes, bytes]:
+    return (self.first, self.second)
+
+
+@dataclass
+class BpeToken:
+  origin: bytes
+  count: int
+  tokens: list[bytes]
+
+  def __repr__(self) -> str:
+    return f"BpeToken({self.origin!r}, count={self.count}, tokens={self.tokens})"
+
+  def __hash__(self) -> int:
+    return hash(self.origin)
+
+
+@dataclass
+class BpePair:
+  first: bytes
+  second: bytes
+  count: int
+  froms_: set[BpeToken]
+
+  def __lt__(self, other: "BpePair") -> bool:
+    if self.count != other.count:
+      return self.count < other.count
+    if self.first != other.first:
+      return self.first < other.first
+    return self.second < other.second
+
+  def to_pair(self) -> tuple[bytes, bytes]:
+    return (self.first, self.second)
+
+  def to_simple(self) -> SimplePair:
+    return SimplePair(self.first, self.second)
+
+  def __repr__(self) -> str:
+    return f"BpePair({self.first!r}, {self.second!r}, count={self.count})\n  from_: {self.froms_}"
--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -6,66 +6,9 @@ import regex
 from collections import Counter
 from multiprocessing import Pool
 from cs336_basics.pretokenization_example import find_chunk_boundaries
-
+from bpe_utils import PAT, merge_tokens, diff_tokens, count_apperence
+from bpe_types import BpeToken, BpePair, SimplePair
 # %%
-PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-PAT = regex.compile(PATTERN)
-# %%
-
-
-@dataclass
-class SimplePair:
-  first: bytes
-  second: bytes
-
-  def concat(self) -> bytes:
-    return self.first + self.second
-
-  def __eq__(self, other: "SimplePair") -> bool:
-    return self.first == other.first and self.second == other.second
-
-  def __hash__(self) -> int:
-    return hash((self.first, self.second))
-
-  def to_pair(self) -> tuple[bytes, bytes]:
-    return (self.first, self.second)
-
-
-@dataclass
-class BpeToken:
-  origin: bytes
-  count: int
-  tokens: list[bytes]
-
-  def __repr__(self) -> str:
-    return f"BpeToken({self.origin!r}, count={self.count}, tokens={self.tokens})"
-
-  def __hash__(self) -> int:
-    return hash(self.origin)
-
-
-@dataclass
-class BpePair:
-  first: bytes
-  second: bytes
-  count: int
-  froms_: set[BpeToken]
-
-  def __lt__(self, other: "BpePair") -> bool:
-    if self.count != other.count:
-      return self.count < other.count
-    if self.first != other.first:
-      return self.first < other.first
-    return self.second < other.second
-
-  def to_pair(self) -> tuple[bytes, bytes]:
-    return (self.first, self.second)
-
-  def to_simple(self) -> SimplePair:
-    return SimplePair(self.first, self.second)
-
-  def __repr__(self) -> str:
-    return f"BpePair({self.first!r}, {self.second!r}, count={self.count})\n  from_: {self.froms_}"
 
 
 def init_bpe_tokens(tokens: Counter[str]) -> list[BpeToken]:
@@ -103,53 +46,6 @@ def init_bpe_pairs(bpe_tokens: list[BpeToken]) -> list[BpePair]:
   return bpe_pairs
 
 # %%
-
-
-def merge_tokens(tokens: list[bytes], target_pair: SimplePair) -> list[bytes]:
-  merged_tokens: list[bytes] = []
-  i = 0
-  while i < len(tokens):
-    if i < len(tokens) - 1 and tokens[i:i + 2] == [target_pair.first, target_pair.second]:
-      merged_tokens.append(target_pair.concat())
-      i += 2
-    else:
-      merged_tokens.append(tokens[i])
-      i += 1
-  return merged_tokens
-
-# %%
-
-
-def diff_tokens(new_tokens: list[bytes], target_pair: SimplePair):
-  to_add = []
-  to_remove = []
-  concat = target_pair.concat()
-  for index, token in enumerate(new_tokens):
-    if token != concat:
-      continue
-    pre_index = index - 1
-    post_index = index + 1
-    if pre_index >= 0 and new_tokens[pre_index] != concat:
-      to_add.append(SimplePair(new_tokens[pre_index], concat))
-      to_remove.append(SimplePair(new_tokens[pre_index], target_pair.first))
-    if post_index < len(new_tokens):
-      to_add.append(SimplePair(concat, new_tokens[post_index]))
-      # 如果后一个token不是concat
-      if new_tokens[post_index] != concat:
-        to_remove.append(SimplePair(target_pair.second, new_tokens[post_index]))
-      # 后一个也是合并生成的
-      else:
-        to_remove.append(SimplePair(target_pair.second, target_pair.first))
-  return to_add, to_remove
-# %%
-
-
-def count_apperence(tokens: list[bytes], pair: SimplePair) -> int:
-  count = 0
-  for first, second in zip(tokens, tokens[1:]):
-    if first == pair.first and second == pair.second:
-      count += 1
-  return count
 
 
 def update_bpe_pairs_dict(bpe_pairs_dict: dict[SimplePair, BpePair], target_bpe_pair: BpePair, vocab_no: int):
