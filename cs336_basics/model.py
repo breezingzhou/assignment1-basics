cca559e5e18789152c6e02f72d07a3540f01401f
breezing <542538469@qq.com.com>
2025-11-26 22:14:28 +0800

    impl MyLinear

---
+++ cs336_basics/model.py
@@ -0,0 +1,30 @@
+# %%
+import math
+import torch
+from torch.nn import Module, Linear, init
+from einops import rearrange, einsum
+
+# %%
+
+
+class MyLinear(Module):
+  in_features: int
+  out_features: int
+  device: torch.device | None
+  dtype: torch.dtype | None
+
+  def __init__(self, in_features: int, out_features: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
+    super().__init__()
+    factory_kwargs = {"device": device, "dtype": dtype}
+    self.in_features = in_features
+    self.out_features = out_features
+    self.weight = torch.nn.Parameter(
+        data=torch.empty((out_features, in_features), **factory_kwargs)
+    )
+    self.reset_parameters()
+
+  def reset_parameters(self) -> None:
+    init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+
+  def forward(self, input: torch.Tensor) -> torch.Tensor:
+    return einsum(self.weight, input, "out_features in_features, ... in_features -> ... out_features")
