3d7d883f7c8363d26ac12f9ea64e5a030de8d6b8
breezing <542538469@qq.com.com>
2025-11-27 23:33:57 +0800

    impl MyRotaryPositionalEmbedding

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -2,7 +2,7 @@
 import math
 import torch
 from torch.nn import Module, Linear, init, Embedding, RMSNorm, SiLU
-from einops import rearrange, einsum, reduce
+from einops import rearrange, einsum, reduce, repeat
 
 # %%
 
@@ -110,3 +110,52 @@ class MySwiGLU(Module):
     FFN(x) = SwiGLU(x,W1,W2,W3) = W2(SiLU(W1x)⊙W3x)
     """
     return self.w2(einsum(SiLU()(self.w1(x)), self.w3(x), "... d_ff, ... d_ff -> ... d_ff"))
+
+
+class MyRotaryPositionalEmbedding(Module):
+  def __init__(self, theta: float, d_k: int, max_seq_len: int, device: torch.device | None = None):
+    assert d_k % 2 == 0, "d_k must be even for RoPE"
+    super().__init__()
+    self.theta = theta
+    self.d_k = d_k
+    self.max_seq_len = max_seq_len
+
+    self.rot_matrix = self.create_rot_matrix(theta, d_k, max_seq_len, device)
+    self.register_buffer("rot_matrix", self.rot_matrix, persistent=False)
+
+  def create_rot_matrix(self, theta: float, d_k: int, max_seq_len: int, device: torch.device | None = None) -> torch.Tensor:
+    """
+    Create the r tensor for RoPE.
+    θ_ik = i / ( θ ** ((2k - 2) / d_k) )  for k = 1, 2, ..., d_k/2
+    """
+    d = d_k // 2
+    i = torch.arange(0, max_seq_len, device=device)
+    k = torch.arange(0, d, device=device)
+    theta_k = 1 / (theta ** (2 * k / d_k))
+    theta_ik = einsum(i, theta_k, "i, k -> i k")  # Shape: (max_seq_len, d_k/2)
+
+    cos_val = torch.cos(theta_ik)
+    sin_val = torch.sin(theta_ik)
+    rot_submat = torch.zeros(*theta_ik.shape, 2, 2, device=device)
+    rot_submat[..., 0, 0] = cos_val  # [d] → [d,1,1]
+    rot_submat[..., 0, 1] = -sin_val
+    rot_submat[..., 1, 0] = sin_val
+    rot_submat[..., 1, 1] = cos_val
+
+    rot_submat_reshape = rearrange(rot_submat, 'i k h w -> i h (k w)')
+    rot_submat_repeat = repeat(rot_submat_reshape, 'i h d_k -> i (d h) d_k', d=d)
+
+    eye = torch.eye(d, device=device)
+    block_eye = repeat(eye, 'i j -> i h j w', h=2, w=2)
+    block_eye = rearrange(block_eye, 'i h j w -> (i h) (j w)')  # 形状: (d_k, d_k)
+
+    rot_matrix = einsum(rot_submat_repeat, block_eye, "... d_k1 d_k2, d_k1 d_k2 -> ... d_k1 d_k2")
+    return rot_matrix
+
+  def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+    """
+    x: (Float[Tensor, "... sequence_length d_k"]): Input tensor to run RoPE on.
+    token_positions: (Int[Tensor, "... sequence_length"]): Tensor of shape (batch_size, sequence_length) with the token positions
+    """
+
+    return einsum(x, self.rot_matrix[token_positions], "... sequence_length d_k2 , ... sequence_length d_k1 d_k2   -> ... sequence_length d_k1")
