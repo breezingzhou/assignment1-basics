91cc91deb3ae31bc93f1e525edf56bd07addb91f
breezing <542538469@qq.com.com>
2025-11-27 16:16:39 +0800

    impl MySwiGLU

--- tests/adapters.py
+++ tests/adapters.py
@@ -12,7 +12,7 @@ from torch import Tensor
 
 from cs336_basics.tokenizer import BpeTokenizer
 from cs336_basics.train_bpe import train_bpe
-from cs336_basics.model import MyLinear, MyEmbedding, MyRMSNorm
+from cs336_basics.model import MyLinear, MyEmbedding, MyRMSNorm, MySwiGLU
 
 
 def run_linear(
@@ -92,7 +92,10 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+
+    swiglu = MySwiGLU(d_model, d_ff)
+    swiglu.load_state_dict({"w1.weight": w1_weight, "w2.weight": w2_weight, "w3.weight": w3_weight})
+    return swiglu(in_features)
 
 
 def run_scaled_dot_product_attention(
