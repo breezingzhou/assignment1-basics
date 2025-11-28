efc5ee3542729aa77e03fd7a66a709bbaf2672a8
breezing <542538469@qq.com.com>
2025-11-29 02:16:32 +0800

    writeup2

--- cs336_basics/writeup2.py
+++ cs336_basics/writeup2.py
@@ -1,6 +1,8 @@
 
 # %%
 from dataclasses import dataclass
+import math
+import humanize
 
 
 @dataclass
@@ -14,43 +16,63 @@ class LMConfig:
 
 
 class Linear():
-  def __init__(self, in_features: int, out_features: int) -> None:
+  def __init__(self, in_features: int, out_features: int, input_shape: list[int]) -> None:
     self.in_features = in_features
     self.out_features = out_features
+    self.input_shape = input_shape
 
   @property
   def param_count(self) -> int:
     return self.in_features * self.out_features
 
+  @property
+  def flops(self) -> int:
+    return 2 * math.prod(self.input_shape) * self.out_features
+
 
 class Embedding():
-  def __init__(self, num_embeddings: int, embedding_dim: int) -> None:
+  def __init__(self, num_embeddings: int, embedding_dim: int, input_shape: list[int]) -> None:
     self.num_embeddings = num_embeddings
     self.embedding_dim = embedding_dim
+    self.input_shape = input_shape
 
   @property
   def param_count(self) -> int:
     return self.num_embeddings * self.embedding_dim
 
+  @property
+  def flops(self) -> int:
+    return 0
 
-class MSNorm():
-  def __init__(self, d_model: int) -> None:
+
+class RMSNorm():
+  def __init__(self, d_model: int, input_shape: list[int]) -> None:
     self.d_model = d_model
+    self.input_shape = input_shape
 
   @property
   def param_count(self) -> int:
     return self.d_model
 
+  @property
+  def flops(self) -> int:
+    prod = math.prod(self.input_shape)
+    return 3 * prod
+
 
 class SwiGLU():
-  def __init__(self, d_model: int, d_ff: int) -> None:
-    self.w1 = Linear(d_model, d_ff)
-    self.w2 = Linear(d_ff, d_model)
-    self.w3 = Linear(d_model, d_ff)
+  def __init__(self, d_model: int, d_ff: int, input_shape: list[int]) -> None:
+    self.w1 = Linear(d_model, d_ff, input_shape)
+    self.w2 = Linear(d_ff, d_model, input_shape[:-1] + [d_ff])
+    self.w3 = Linear(d_model, d_ff, input_shape)
 
   @property
   def param_count(self) -> int:
     return self.w1.param_count + self.w2.param_count + self.w3.param_count
+
+  @property
+  def flops(self) -> int:
+    return self.w1.flops + self.w2.flops + self.w3.flops
 
 
 class RotaryPositionalEmbedding():
@@ -61,14 +83,19 @@ class RotaryPositionalEmbedding():
   def param_count(self) -> int:
     return 0
 
+  @property
+  def flops(self) -> int:
+    return 0
+
 
 class MultiHeadSelfAttention():
-  def __init__(self, d_model: int, num_heads: int) -> None:
+  def __init__(self, d_model: int, num_heads: int, input_shape: list[int]) -> None:
+    self.d_model = d_model
     self.rope = RotaryPositionalEmbedding()
-    self.q_proj = Linear(d_model, d_model)
-    self.k_proj = Linear(d_model, d_model)
-    self.v_proj = Linear(d_model, d_model)
-    self.output_proj = Linear(d_model, d_model)
+    self.q_proj = Linear(d_model, d_model, input_shape)
+    self.k_proj = Linear(d_model, d_model, input_shape)
+    self.v_proj = Linear(d_model, d_model, input_shape)
+    self.output_proj = Linear(d_model, d_model, input_shape)
 
   @property
   def param_count(self) -> int:
@@ -77,13 +104,19 @@ class MultiHeadSelfAttention():
         self.output_proj.param_count + self.rope.param_count
     )
 
+  @property
+  def flops(self) -> int:
+    scaled_dot_product_attention = 2 * math.prod(self.q_proj.input_shape[:-1]) * self.d_model
+    return scaled_dot_product_attention + self.q_proj.flops + self.k_proj.flops + self.v_proj.flops + self.output_proj.flops + self.rope.flops * 2
+
 
 class TransformerBlock():
-  def __init__(self, d_model: int, num_heads: int, d_ff: int,) -> None:
-    self.attn = MultiHeadSelfAttention(d_model, num_heads)
-    self.ln1 = MSNorm(d_model)
-    self.ffn = SwiGLU(d_model, d_ff)
-    self.ln2 = MSNorm(d_model)
+  def __init__(self, d_model: int, num_heads: int, d_ff: int, input_shape: list[int]) -> None:
+    # input_shape: [sequence_length, d_model]
+    self.attn = MultiHeadSelfAttention(d_model, num_heads, input_shape)
+    self.ln1 = RMSNorm(d_model, input_shape)
+    self.ffn = SwiGLU(d_model, d_ff, input_shape)
+    self.ln2 = RMSNorm(d_model, input_shape)
 
   @property
   def param_count(self) -> int:
@@ -91,16 +124,23 @@ class TransformerBlock():
         self.attn.param_count + self.ln1.param_count + self.ffn.param_count + self.ln2.param_count
     )
 
+  @property
+  def flops(self) -> int:
+    return self.attn.flops + self.ln1.flops + self.ffn.flops + self.ln2.flops
+
 
 class TransformerLM():
   def __init__(self, config: LMConfig) -> None:
-    self.token_embedding = Embedding(config.vocab_size, config.d_model)
+    self.token_embedding = Embedding(config.vocab_size, config.d_model,
+                                     input_shape=[config.context_length])
     self.layers = [
-        TransformerBlock(config.d_model, config.num_heads, config.d_ff)
+        TransformerBlock(config.d_model, config.num_heads, config.d_ff,
+                         input_shape=[config.context_length, config.d_model])
         for _ in range(config.num_layers)
     ]
-    self.ln_final = MSNorm(config.d_model)
-    self.lm_head = Linear(config.d_model, config.vocab_size)
+    self.ln_final = RMSNorm(config.d_model, input_shape=[config.context_length, config.d_model])
+    self.lm_head = Linear(config.d_model, config.vocab_size, input_shape=[
+                          config.context_length, config.d_model])
 
   @property
   def param_count(self) -> int:
@@ -109,6 +149,44 @@ class TransformerLM():
         sum(layer.param_count for layer in self.layers) + self.ln_final.param_count +
         self.lm_head.param_count
     )
+
+  def display_param_count(self) -> None:
+    layer_param_count = self.layers[0].param_count
+    print(f"| Component | Parameters |")
+    print(f"|----------|-------|")
+    print(f"| Token Embedding | {self.token_embedding.param_count} |")
+    print(f"| Transformer Block | {layer_param_count} |")
+    print(f"| Transformer Block Total | {layer_param_count * len(self.layers)} |")
+    print(f"| Final LayerNorm | {self.ln_final.param_count} |")
+    print(f"| LM Head | {self.lm_head.param_count} |")
+    print(f"| Total Parameters | {self.param_count} |")
+
+  @property
+  def flops(self) -> int:
+    return (
+        self.token_embedding.flops +
+        sum(layer.flops for layer in self.layers) + self.ln_final.flops +
+        self.lm_head.flops
+    )
+
+  def display_flops(self) -> None:
+    token_embedding = self.token_embedding.flops
+    layer_flops = self.layers[0].flops
+    layers_flops = layer_flops * len(self.layers)
+    ln_final = self.ln_final.flops
+    lm_head = self.lm_head.flops
+    total_flops = token_embedding + layers_flops + ln_final + lm_head
+
+    print(f"| Component | FLOPs | rate |")
+    print(f"|----------|-------|")
+    print(
+        f"| Token Embedding | {humanize.intword(token_embedding)} | {token_embedding / total_flops} |")
+    print(f"| Transformer Block | {humanize.intword(layer_flops)} | {layer_flops / total_flops} |")
+    print(
+        f"| Transformer Block Total | {humanize.intword(layers_flops)} | {layers_flops / total_flops} |")
+    print(f"| Final LayerNorm | {humanize.intword(ln_final)} | {ln_final / total_flops} |")
+    print(f"| LM Head | {humanize.intword(lm_head)} | {lm_head / total_flops} |")
+    print(f"| Total FLOPs | {humanize.intword(total_flops)} | 1 |")
 
 
 # %%
@@ -121,4 +199,60 @@ config = LMConfig(
     d_ff=6400,
 )
 lm = TransformerLM(config)
-lm.param_count
+
+lm.display_param_count()
+print("")
+lm.display_flops()
+# %%
+small_config = LMConfig(
+    vocab_size=50257,
+    context_length=1024,
+    num_layers=12,
+    d_model=768,
+    num_heads=12,
+    d_ff=6400,
+)
+medium_config = LMConfig(
+    vocab_size=50257,
+    context_length=1024,
+    num_layers=24,
+    d_model=1024,
+    num_heads=16,
+    d_ff=6400,
+)
+large_config = LMConfig(
+    vocab_size=50257,
+    context_length=1024,
+    num_layers=36,
+    d_model=1280,
+    num_heads=20,
+    d_ff=6400,
+)
+lm_small = TransformerLM(small_config)
+lm_medium = TransformerLM(medium_config)
+lm_large = TransformerLM(large_config)
+print("Small Model:")
+lm_small.display_flops()
+print("")
+print("Medium Model:")
+lm_medium.display_flops()
+print("")
+print("Large Model:")
+lm_large.display_flops()
+# %%
+context_length_config = LMConfig(
+    vocab_size=50257,
+    context_length=16384,
+    num_layers=48,
+    d_model=1600,
+    num_heads=25,
+    d_ff=6400,
+)
+
+lm_context_length = TransformerLM(context_length_config)
+
+print("Original Model:")
+lm.display_flops()
+print("")
+print("Context Length Model:")
+lm_context_length.display_flops()
--- writeup.md
+++ writeup.md
@@ -70,17 +70,88 @@ BPE Training最耗时的部分是 选择出现频率最高的pair  (max函数)
   * d_model : 1,600
   * num_heads : 25
   * d_ff : 6,400
+总共需要约2.12b的参数,需要内存68GB
+| Part                    | params_num |
+| ----------------------- | ---------- |
+| Token Embedding         | 80411200   |
+| Transformer Block       | 40963200   |
+| Transformer Block Total | 1966233600 |
+| Final LayerNorm         | 1600       |
+| LM Head                 | 80411200   |
+| Total Parameters        | 2127057600 |
 
 
-###  Identify the matrix multiplies required to complete a forward pass of our GPT-2XL-shaped model. How many FLOPs do these matrix multiplies require intotal? Assume that our input sequence has context_length tokens.
+###  Identify the matrix multiplies required to complete a forward pass of our GPT-2XL-shaped model. How many FLOPs do these matrix multiplies require in total? Assume that our input sequence has context_length tokens.
+
+总共需要约4.2t FLOPs
+| Component               | FLOPs         |
+| ----------------------- | ------------- |
+| Token Embedding         | 0             |
+| Transformer Block       | 83.9 billion  |
+| Transformer Block Total | 4.0 trillion  |
+| Final LayerNorm         | 4.9 million   |
+| LM Head                 | 164.7 billion |
+| Total FLOPs             | 4.2 trillion  |
 
 
 
 ### Based on your analysis above, which parts of the model require the most FLOPs?
 
+Transformer Blocks
 
-### Repeat your analysis withGPT-2 small (12layers, 768d_model, 12heads), GPT-2 medium(24 layers, 1024d_model, 16heads), and GPT-2large(36layers, 1280d_model, 20heads). As the model size increases, which parts of the Transformer LM take up proportionally more or less of the total FLOPs?
+### Repeat your analysis with GPT-2 small (12 layers, 768 d_model, 12 heads), GPT-2 medium(24 layers, 1024 d_model, 16 heads), and GPT-2 large(36 layers, 1280 d_model, 20 heads). As the model size increases, which parts of the Transformer LM take up proportionally more or less of the total FLOPs?
 
+Transformer Blocks的占比越来越多
+Final LayerNorm 和 LM Head的占比越来越少
+Small Model:
+| Component               | FLOPs         | rate                   |
+| ----------------------- | ------------- |
+| Token Embedding         | 0             | 0.0                    |
+| Transformer Block       | 35.0 billion  | 0.0701450544052423     |
+| Transformer Block Total | 420.4 billion | 0.8417406528629076     |
+| Final LayerNorm         | 2.4 million   | 4.7233606396060085e-06 |
+| LM Head                 | 79.0 billion  | 0.15825462377645277    |
+| Total FLOPs             | 499.5 billion | 1                      |
 
+Medium Model:
+| Component               | FLOPs         | rate                   |
+| ----------------------- | ------------- |
+| Token Embedding         | 0             | 0.0                    |
+| Transformer Block       | 48.9 billion  | 0.03823065885536095    |
+| Transformer Block Total | 1.2 trillion  | 0.9175358125286628     |
+| Final LayerNorm         | 3.1 million   | 2.4612012138644385e-06 |
+| LM Head                 | 105.4 billion | 0.0824617262701234     |
+| Total FLOPs             | 1.3 trillion  | 1                      |
+
+Large Model:
+| Component               | FLOPs         | rate                  |
+| ----------------------- | ------------- |
+| Token Embedding         | 0             | 0.0                   |
+| Transformer Block       | 63.8 billion  | 0.026270017199063637  |
+| Transformer Block Total | 2.3 trillion  | 0.9457206191662909    |
+| Final LayerNorm         | 3.9 million   | 1.620005994022178e-06 |
+| LM Head                 | 131.7 billion | 0.054277760827715064  |
+| Total FLOPs             | 2.4 trillion  | 1                     |
 
 ### Take GPT-2 XL and increase the context length to 16,384. How does the total FLOPs for one forward pass change? How do the relative contribution of FLOPs of the model components change?
+总FLOPs从4.2t上升到67.1t。相对占比保持不变。
+
+Original Model:
+| Component               | FLOPs         | rate                   |
+| ----------------------- | ------------- |
+| Token Embedding         | 0             | 0.0                    |
+| Transformer Block       | 83.9 billion  | 0.020014844629726546   |
+| Transformer Block Total | 4.0 trillion  | 0.9607125422268743     |
+| Final LayerNorm         | 4.9 million   | 1.1725615897746376e-06 |
+| LM Head                 | 164.7 billion | 0.03928628521153597    |
+| Total FLOPs             | 4.2 trillion  | 1                      |
+
+Context Length Model:
+| Component               | FLOPs         | rate                   |
+| ----------------------- | ------------- |
+| Token Embedding         | 0             | 0.0                    |
+| Transformer Block       | 1.3 trillion  | 0.020014844629726546   |
+| Transformer Block Total | 64.4 trillion | 0.9607125422268743     |
+| Final LayerNorm         | 78.6 million  | 1.1725615897746376e-06 |
+| LM Head                 | 2.6 trillion  | 0.03928628521153597    |
+| Total FLOPs             | 67.1 trillion | 1                      |
