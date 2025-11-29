a5be71559edd72773ae5e3f61826d4a749e376cf
breezing <542538469@qq.com.com>
2025-11-29 15:25:45 +0800

    impl MyAdamW

--- cs336_basics/optimizer.py
+++ cs336_basics/optimizer.py
@@ -34,13 +34,46 @@ class MySGD(Optimizer):
   def step(self, closure: Callable | None = None):
     loss = None if closure is None else closure()
     for group in self.param_groups:
-      lr = group["lr"]  # Get the learning rate
+      lr: float = group["lr"]  # Get the learning rate
       for p in group["params"]:
         if p.grad is None:
           continue
-      state = self.state[p]  # Get state associated with p.
-      t = state.get("t", 0)  # Get iteration number from the state, or initial value.
-      grad = p.grad.data  # Get the gradient of loss with respect to p.
-      p.data -= lr / math.sqrt(t + 1) * grad  # Update weight tensor in-place.
-      state["t"] = t + 1  # Increment iteration number
+        state = self.state[p]  # Get state associated with p.
+        t = state.get("t", 0)  # Get iteration number from the state, or initial value.
+        grad = p.grad.data  # Get the gradient of loss with respect to p.
+        p.data -= lr / math.sqrt(t + 1) * grad  # Update weight tensor in-place.
+        state["t"] = t + 1  # Increment iteration number
+    return loss
+
+
+class MyAdamW(Optimizer):
+  def __init__(self, params: ParamsT, lr: float = 1e-3, weight_decay: float = 0.01,
+               betas: tuple[float, float] = (0.9, 0.999), eps: float = 1e-8,):
+    defaults = dict(lr=lr, weight_decay=weight_decay, betas=betas, eps=eps)
+    super().__init__(params, defaults)
+    self.m = 0.0
+    self.v = 0.0
+
+  def step(self, closure: Callable | None = None):
+    loss = None if closure is None else closure()
+    for group in self.param_groups:
+      lr: float = group["lr"]
+      betas: tuple[float, float] = group["betas"]
+      eps = group["eps"]
+      weight_decay = group["weight_decay"]
+      for p in group["params"]:
+        p: Tensor
+        if p.grad is None:
+          continue
+        grad = p.grad.data  # Get the gradient of loss with respect to p.
+        self.m = betas[0] * self.m + (1 - betas[0]) * grad  # Update first moment estimate
+        self.v = betas[1] * self.v + (1 - betas[1]) * grad ** 2  # Update second moment estimate
+
+        state = self.state[p]  # Get state associated with p.
+        t = state.get("t", 0) + 1  # Get iteration number from the state, or initial value.
+
+        new_lr = lr * math.sqrt(1 - betas[1]**t) / (1 - betas[0] ** t)  # Adjust learning rate
+        p.data -= new_lr * self.m / (torch.sqrt(self.v) + eps)  # Update weight tensor in-place
+        p.data -= lr * weight_decay * p.data  # Apply weight decay
+        state["t"] = t  # Increment iteration number
     return loss
--- tests/adapters.py
+++ tests/adapters.py
@@ -11,7 +11,7 @@ from torch import Tensor
 
 
 from cs336_basics.nn_utils import my_scaled_dot_product_attention, my_silu, my_softmax
-from cs336_basics.optimizer import my_cross_entropy
+from cs336_basics.optimizer import MyAdamW, my_cross_entropy
 from cs336_basics.tokenizer import BpeTokenizer
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.model import MyLinear, MyEmbedding, MyMultiHeadSelfAttention, MyRMSNorm, MyRotaryPositionalEmbedding, MySwiGLU, MyTransformerBlock, MyTransformerLM
@@ -505,7 +505,7 @@ def get_adamw_cls() -> Any:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return MyAdamW
 
 
 def run_get_lr_cosine_schedule(
