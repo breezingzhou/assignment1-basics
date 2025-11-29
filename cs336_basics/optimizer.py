7b5f2d6b12386efb2f6df0b037501ad48b491580
breezing <542538469@qq.com.com>
2025-11-29 15:50:01 +0800

    refactor

--- cs336_basics/optimizer.py
+++ cs336_basics/optimizer.py
@@ -10,28 +10,6 @@ from torch.optim.optimizer import ParamsT
 from collections.abc import Callable, Iterable
 
 
-from cs336_basics.nn_utils import my_softmax
-# %%-
-
-
-def my_cross_entropy(inputs: Float[Tensor, " batch_size vocab_size"], targets: Int[Tensor, " batch_size"]) -> Float[Tensor, ""]:
-  max_ = inputs.max(dim=-1, keepdim=True).values
-  logsumexp = torch.log(torch.exp(inputs - max_).sum(dim=-1, keepdim=True)) + max_
-  log_probs = inputs - logsumexp
-  batch_indices = torch.arange(inputs.size(0), device=inputs.device)
-  target_log_probs = log_probs[batch_indices, targets]
-  return -target_log_probs.mean()
-
-
-def my_get_lr_cosine_schedule(it: int, max_learning_rate: float, min_learning_rate: float, warmup_iters: int, cosine_cycle_iters: int) -> float:
-  if it < warmup_iters:
-    return max_learning_rate * it / warmup_iters
-  elif it > cosine_cycle_iters:
-    return min_learning_rate
-  else:
-    cos_inner = math.pi * (it - warmup_iters) / (cosine_cycle_iters - warmup_iters)
-    return min_learning_rate + 0.5 * (1 + math.cos(cos_inner)) * (max_learning_rate - min_learning_rate)
-
 # %%
 
 
