62fa48979ec609bd0344de7f4ba3ddb243d56ff2
breezing <542538469@qq.com.com>
2025-12-04 22:31:41 +0800

    validate and inferance

--- scripts/bpe_encode.py
+++ scripts/bpe_encode.py
@@ -7,43 +7,12 @@ from collections.abc import Generator
 import time
 from cs336_basics.pretokenization_example import find_chunk_boundaries
 from cs336_basics.tokenizer import BpeTokenizer
-from tests.common import gpt2_bytes_to_unicode
-from common import OUTPUT_DIR, DATA_DIR
+from common import OUTPUT_DIR, DATA_DIR, get_tokenizer_from_vocab_merges_path
 from pathlib import Path
 import numpy as np
 import json
 import math
 # %%
-
-
-def get_tokenizer_from_vocab_merges_path(
-    vocab_path: Path,
-    merges_path: Path,
-    special_tokens: list[str] | None = None,
-):
-  gpt2_byte_decoder = {v: k for k, v in gpt2_bytes_to_unicode().items()}
-  with open(vocab_path) as vocab_f:
-    gpt2_vocab: dict[int, str] = json.load(vocab_f)
-
-  gpt2_merges = []
-  with open(merges_path) as merges_f:
-    for line in merges_f:
-      cleaned_line = line.rstrip()
-      if cleaned_line and len(cleaned_line.split(" ")) == 2:
-        gpt2_merges.append(tuple(cleaned_line.split(" ")))
-
-  vocab = {
-      index: bytes([gpt2_byte_decoder[token] for token in item])
-      for index, item in gpt2_vocab.items()
-  }
-  merges = [
-      (
-          bytes([gpt2_byte_decoder[token] for token in merge_token_1]),
-          bytes([gpt2_byte_decoder[token] for token in merge_token_2]),
-      )
-      for merge_token_1, merge_token_2 in gpt2_merges
-  ]
-  return BpeTokenizer(vocab, merges, special_tokens=special_tokens)
 
 
 def get_contents(input_file: Path) -> str:
