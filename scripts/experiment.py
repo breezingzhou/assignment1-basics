8edfa020439768bb93ac7d32bbe390540f06f301
breezing <542538469@qq.com.com>
2025-12-05 18:05:35 +0800

    move config define to common

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -1,9 +1,9 @@
 # %%
 
-from dataclasses import asdict, dataclass
+from dataclasses import asdict
 from cs336_basics.model import MyTransformerLM
 from cs336_basics.optimizer import MyAdamW, MyCosineAnnealingLR
-from cs336_basics.nn_utils import my_cross_entropy, my_get_batch, my_save_checkpoint, my_load_checkpoint, my_get_lr_cosine_schedule, my_gradient_clipping
+from cs336_basics.nn_utils import my_cross_entropy, my_get_batch, my_save_checkpoint, my_load_checkpoint, my_gradient_clipping
 from cs336_basics.tokenizer import BpeTokenizer
 from tests.test_tokenizer import get_tokenizer_from_vocab_merges_path
 import torch
@@ -11,101 +11,7 @@ import numpy as np
 import wandb
 from datetime import datetime
 from pathlib import Path
-from common import OUTPUT_DIR, EXPERIMENT_DIR, CHECKPOINT_FINAL_NAME
-import toml
-# %%
-
-
-@dataclass
-class ModelHyperParams:
-  vocab_size: int
-  context_length: int
-  d_model: int
-  num_layers: int
-  num_heads: int
-  d_ff: int
-  rope_theta: float
-
-
-@dataclass
-class OptimizerHyperParams:
-  learning_rate: float
-  weight_decay: float
-  betas: tuple[float, float]
-  eps: float
-
-  def __post_init__(self):
-    if isinstance(self.betas, list):
-      self.betas = (self.betas[0], self.betas[1])
-
-
-@dataclass
-class SechduleParams:
-  min_lr_coeff: float
-  warmup_iters: int
-  cosine_cycle_iters: int
-
-
-@dataclass
-class ClippingParams:
-  max_l2_norm: float = 1e-2
-
-
-@dataclass
-class TrainConfig:
-  module_params: ModelHyperParams
-  optimizer_params: OptimizerHyperParams
-  schedule_params: SechduleParams | None
-  clipping_params: ClippingParams
-
-  train_epochs: int
-  eval_epochs: int
-  batch_size: int
-
-  dataset_name: str
-  name: str
-  save_every_n_epochs: int
-
-  @property
-  def checkpoint_dir(self) -> Path:
-    return EXPERIMENT_DIR / self.name / "checkpoints"
-
-  @property
-  def experiment_dir(self) -> Path:
-    return EXPERIMENT_DIR / self.name
-
-# %%
-
-
-def load_config(path: Path) -> TrainConfig:
-  with open(path, 'r') as f:
-    config_dict = toml.load(f)
-  module_params = ModelHyperParams(**config_dict['module_params'])
-  optimizer_params = OptimizerHyperParams(**config_dict['optimizer_params'])
-  schedule_params = SechduleParams(
-      **config_dict['schedule_params']) if 'schedule_params' in config_dict and config_dict['schedule_params'] is not None else None
-  clipping_params = ClippingParams(**config_dict['clipping_params'])
-  config = TrainConfig(
-      module_params=module_params,
-      optimizer_params=optimizer_params,
-      schedule_params=schedule_params,
-      clipping_params=clipping_params,
-      train_epochs=config_dict['train_epochs'],
-      eval_epochs=config_dict['eval_epochs'],
-      batch_size=config_dict['batch_size'],
-      dataset_name=config_dict['dataset_name'],
-      name=config_dict['name'],
-      save_every_n_epochs=config_dict['save_every_n_epochs'],
-  )
-  return config
-
-
-def save_config(config: TrainConfig, path: Path):
-  path.parent.mkdir(parents=True, exist_ok=True)
-  with open(path, 'w') as f:
-    toml.dump(asdict(config), f)
-
-
+from common import OUTPUT_DIR, EXPERIMENT_DIR, CHECKPOINT_FINAL_NAME, ClippingParams, ModelHyperParams, OptimizerHyperParams, SechduleParams, TrainConfig, save_config, load_config
 # %%
 
 
