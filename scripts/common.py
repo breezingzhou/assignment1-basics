7c899824c539c6e7deb2dac52dfce4b54ad630c4
breezing <542538469@qq.com.com>
2025-12-05 14:49:47 +0800

    rename

--- scripts/common.py
+++ scripts/common.py
@@ -7,7 +7,7 @@ from pathlib import Path
 WORKSPACE = Path(__file__).parent.parent
 OUTPUT_DIR = WORKSPACE / "output"
 DATA_DIR = WORKSPACE / "data"
-CHECKPOINTS_DIR = WORKSPACE / "checkpoints"
+EXPERIMENT_DIR = WORKSPACE / "experiment"
 
 
-__all__ = ["WORKSPACE", "OUTPUT_DIR", "DATA_DIR", "CHECKPOINTS_DIR",]
+__all__ = ["WORKSPACE", "OUTPUT_DIR", "DATA_DIR", "EXPERIMENT_DIR",]
